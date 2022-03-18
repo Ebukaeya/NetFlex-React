e0d890a39a0951c6f5767a9cc651f56f47a3eeb7
giorgio <giorgiolink94@hotmail.it>
2022-03-18 15:57:05 +0000

    Update

--- netflex_react/src/App.js
+++ netflex_react/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css"
 import Footer from "./Components/Footer"
+import NavBar from "./Components/Nabar"
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Footer />
     </div>
   )
