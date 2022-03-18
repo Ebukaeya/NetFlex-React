305fb87310091d7de5f7af5338091f699f65b9d7
Amin Atashkhayer <amin.atashin@gmail.com>
2022-03-18 16:23:32 +0100

    Update Main.jsx

--- netflex_react/src/Component/Main.jsx
+++ netflex_react/src/Component/Main.jsx
@@ -5,8 +5,11 @@ import Middle from "./Middle";
 
 const Main = () => (
   <>
+    <h3>Lord Of The Rings</h3>
     <Middle saga={"the+lord+of+the+rings"} />
+    <h3>Harry Potter</h3>
     <Middle saga={"harry+potter"} />
+    <h3>Terminator</h3>
     <Middle saga={"terminator"} />
   </>
 );
