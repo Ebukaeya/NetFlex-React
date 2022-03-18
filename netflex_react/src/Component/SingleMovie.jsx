1cf15297b7b475d41f2ae70c273ce0b444afcf13
Amin Atashkhayer <amin.atashin@gmail.com>
2022-03-18 16:42:58 +0100

    Update SingleMovie.jsx

--- netflex_react/src/Component/SingleMovie.jsx
+++ netflex_react/src/Component/SingleMovie.jsx
@@ -2,10 +2,13 @@ import { Card } from "react-bootstrap";
 const SingleMovie = (props) => (
   <>
     <Card key={props.data.imdbID}>
-      <Card.Img variant="top" src={props.data.Poster} />
-      <Card.Body>
-        <Card.Title>{props.Title}</Card.Title>
-      </Card.Body>
+      <Card.Img
+        className="   
+    img-fluid"
+        variant="top"
+        src={props.data.Poster}
+      />
+      <Card.Body>{props.data.Title}</Card.Body>
     </Card>
   </>
 );
